8192de4b62b362b0ad613433a76559f884398519
aloha3307 <77537668+aloha3307@users.noreply.github.com>
2024-10-16 20:29:59 +0800

    Update hand_protector_V2.py

--- hand_protector_V2.py
+++ hand_protector_V2.py
@@ -4,18 +4,7 @@ from time import sleep,time
 from Modules.WindowMgr import *
 from os import system
 
-#讀入INI
-ini_filename="hand_protector_V2.ini"
-if len(argv)>1 and argv[1]!="" and (argv[1]).split(".")[1]=="ini":
-    ini_filename=argv[1]
-#讀取 xinput.ini參數
-with open(ini_filename,"r",encoding="utf-8") as f:
-    tmp_content=f.read()
-exec(tmp_content)
-loop_flag=True
-edit_flag=False
-COMBINATION = {keyboard.Key.alt, keyboard.Key.ctrl,keyboard.Key.enter}
-current = set()
+
 
 def kb_press_eval_key(key_val):
     global edit_flag
@@ -78,33 +67,64 @@ def on_release(key):
     except:
         pass
 
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
+def press_and_release(arg):
+    kb_press_eval_key(arg)
+    kb_release_eval_key(arg)
 
-w=WindowMgr()
-keyon_title='::::KEYON::::'
-w.set_cmd_title(keyon_title)
-w.reset()
-w.set_window_on_top(keyon_title,350,150)
-w.set_window_alpha(keyon_title, alpha_val=190)
-#print(keyon_title)
-if ADD_ALL_KEY_ONOFF_ON_START:
-    for i in KEY_ONOFF: current.add(i)
-print("KEY_ON 監控視窗:"+ACTIVE_WIN_TITLE+"\n按 [Delete] 結束程式 | 按 [\\] 暫停或重啟\n"+"-"*50)
-last_seconds=int(time())
-while 1:
-    if 'stop' in(str(listener)): exit()
-    print_str=f'啟用狀態:{loop_flag} | ON:'+str([i for i in current])+" "*10
-    print('\r'+print_str,end='',flush=True)
-    if ACTIVE_WIN_TITLE=="" or (ACTIVE_WIN_TITLE!="" and ACTIVE_WIN_TITLE in w.active_window_title() and loop_flag):
-        current_seconds=int(time())
-        for i in current:
-            if not KEY_HOLD[i] and current_seconds==last_seconds: continue
-            kb_press_eval_key(i)
-            kb_release_eval_key(i)
-        last_seconds=current_seconds
-        edit_flag=True
-    sleep(DELAY_SECOND)
-    #system("cls")
+
+if __name__=="__main__":
+    #讀入INI
+    ini_filename="hand_protector_V2.ini"
+    if len(argv)>1 and argv[1]!="" and (argv[1]).split(".")[1]=="ini":
+        ini_filename=argv[1]
+    #讀取 xinput.ini參數
+    with open(ini_filename,"r",encoding="utf-8") as f:
+        tmp_content=f.read()
+    exec(tmp_content)
+    loop_flag=True
+    edit_flag=False
+    COMBINATION = {keyboard.Key.alt, keyboard.Key.ctrl,keyboard.Key.enter}
+    current = set()
+    
+    listener = keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release)
+    listener.start()
+    
+    w=WindowMgr()
+    keyon_title='::::KEYON::::'
+    w.set_cmd_title(keyon_title)
+    w.reset()
+    w.set_window_on_top(keyon_title,350,150)
+    w.set_window_alpha(keyon_title, alpha_val=190)
+    #print(keyon_title)
+    if ADD_ALL_KEY_ONOFF_ON_START:
+        for i in KEY_ONOFF: current.add(i)
+    print("KEY_ON 監控視窗:"+ACTIVE_WIN_TITLE+"\n按 [Delete] 結束程式 | 按 [\\] 暫停或重啟\n"+"-"*50)
+    last_seconds=int(time())
+    tmp = set()
+    while 1:
+        PRINT_VAR=""
+        for i in current:PRINT_VAR=PRINT_VAR+" "+i
+        if HEAL_ON: PRINT_VAR+=" 補血"
+        if FORCE_MOVE: PRINT_VAR+=" 移動"
+        if 'stop' in(str(listener)): exit()
+        print_str=f'啟用狀態:{loop_flag} | ON:{PRINT_VAR}'+" "*10
+        print('\r'+print_str,end='',flush=True)
+        if ACTIVE_WIN_TITLE=="" or (ACTIVE_WIN_TITLE!="" and ACTIVE_WIN_TITLE in w.active_window_title() and loop_flag):
+            current_seconds=int(time())
+            if HEAL_ON and current_seconds!=last_seconds:
+                press_and_release(KEY_HEAL)
+            for i in current:
+                if i not in tmp:
+                    press_and_release(i)
+                    tmp.add(i)
+                    break
+            if FORCE_MOVE:
+                press_and_release(KEY_FORCE_MOVE)
+            last_seconds=current_seconds
+            edit_flag=True
+            if tmp == current:
+                for i in current: tmp.remove(i)
+        #system("cls")
+        sleep(DELAY_SECOND)
